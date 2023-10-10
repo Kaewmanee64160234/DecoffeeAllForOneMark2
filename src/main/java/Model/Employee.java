81bd3d111329b7396120d7f98656f01c24c94b6c
Pornchitar Sarsai <kream21574@gmail.com>
2023-10-10 22:32:31 +0700

    AddTestEmployeeService

--- src/main/java/Model/Employee.java
+++ src/main/java/Model/Employee.java
@@ -21,8 +21,9 @@ public class Employee {
     private String email;
     private String position;
     private int hourlyWage;
+    private int user_id;
 
-    public Employee(int id, String name, String address, String telephone, String email, String position, int hourlyWage) {
+    public Employee(int id, String name, String address, String telephone, String email, String position, int hourlyWage, int user_id) {
         this.id = id;
         this.name = name;
         this.address = address;
@@ -30,9 +31,10 @@ public class Employee {
         this.email = email;
         this.position = position;
         this.hourlyWage = hourlyWage;
+        this.user_id = user_id;
     }
-    
-    public Employee(String name, String address, String telephone, String email, String position, int hourlyWage) {
+
+    public Employee(String name, String address, String telephone, String email, String position, int hourlyWage, int user_id) {
         this.id = -1;
         this.name = name;
         this.address = address;
@@ -40,9 +42,10 @@ public class Employee {
         this.email = email;
         this.position = position;
         this.hourlyWage = hourlyWage;
+        this.user_id = user_id;
     }
     
-    public Employee(){
+    public Employee() {
         this.id = -1;
         this.name = "";
         this.address = "";
@@ -50,6 +53,7 @@ public class Employee {
         this.email = "";
         this.position = "";
         this.hourlyWage = 0;
+        this.user_id = -1;
     }
 
     public int getId() {
@@ -108,12 +112,18 @@ public class Employee {
         this.hourlyWage = hourlyWage;
     }
 
-    @Override
-    public String toString() {
-        return "Employee{" + "id=" + id + ", name=" + name + ", address=" + address + ", telephone=" + telephone + ", email=" + email + ", position=" + position + ", hourlyWage=" + hourlyWage + '}';
+    public int getUser_id() {
+        return user_id;
     }
 
-    
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "id=" + id + ", name=" + name + ", address=" + address + ", telephone=" + telephone + ", email=" + email + ", position=" + position + ", hourlyWage=" + hourlyWage + ", user_id=" + user_id + '}';
+    }
     
     public static Employee fromRS(ResultSet rs) {
         Employee employee = new Employee();
@@ -125,7 +135,7 @@ public class Employee {
           employee.setEmail(rs.getString("employee_email"));
           employee.setPosition(rs.getString("employee_position"));
           employee.setHourlyWage(rs.getInt("employee_hourly_wage"));
-            
+          employee.setUser_id(rs.getInt("user_id"));
         } catch (SQLException ex) {
             Logger.getLogger(Employee.class.getName()).log(Level.SEVERE, null, ex);
             return null;

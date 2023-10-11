8fd6f3b99c7059846d253dd6bc629bdd326655ec
Supanut Thamsaard <64160147@go.buu.ac.th>
2023-10-11 15:59:43 +0700

    TestPromotionService

---
+++ src/main/java/TestService/TestPromotionService.java
@@ -0,0 +1,22 @@
+package TestService;
+
+import Dao.PromotionDao;
+import Model.Promotion;
+
+public class TestPromotionService {
+
+    public static void main(String[] args) {
+        PromotionDao pd = new PromotionDao();
+        for (Promotion promotion : pd.getAll()) {
+            System.out.println(promotion);
+        }
+
+        // Add New Promotion
+        Promotion P1 = new Promotion("2023-08-01", "HalloweenDay", 0, 5, 0);
+        pd.save(P1);
+        for (Promotion promotion : pd.getAll()) {
+            System.out.println(promotion);
+        }
+
+    }
+}

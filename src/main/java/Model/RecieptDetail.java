75148db5bf598855e9361286815c27b7701b64bf
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-10-14 17:00:39 +0700

    🔥create attbute Reciept Detail 🔥

---
+++ src/main/java/Model/RecieptDetail.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author USER
+ */
+public class RecieptDetail {
+    private int id;
+    private String name;
+    private int qty;
+    private float price;
+    private String size;
+    private float typePrice;
+    private String type;
+    private float sizePrice;
+    private String topping;
+    private float toppingPrice;
+    private float total;
+    private int recieptId;
+    private int productId;
+    private Product product;
+    private Reciept reciept;
+}

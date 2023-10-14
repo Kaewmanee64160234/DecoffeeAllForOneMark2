c29c79ea9420c4618148b844bd2181f488ad67e2
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-10-14 16:52:30 +0700

    create Reciept

---
+++ src/main/java/Model/Reciept.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author USER
+ */
+public class Reciept {
+    private int id;
+    private Date creaetedDate;
+    private int queue;
+    private float discount;
+    private float total;
+    private float receive;
+    private float change;
+    private int totalQTY;
+    private String payment;
+    private int storeId;
+    private int customerId;
+    private int promotionId;
+    private Customer customer;
+    private Promotion promotion;
+    
+    
+}

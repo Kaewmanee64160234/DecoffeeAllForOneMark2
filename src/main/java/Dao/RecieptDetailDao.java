321e14a13e1766bad404c877ec501d5bfbd2d98a
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-10-14 19:19:56 +0700

    create RecieptDetailDao

---
+++ src/main/java/Dao/RecieptDetailDao.java
@@ -0,0 +1,174 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dao;
+
+import Model.RecieptDetail;
+import helper.DatabaseHelper;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author USER
+ */
+public class RecieptDetailDao implements Dao<RecieptDetail>{
+
+    @Override
+    public RecieptDetail get(int id) {
+        RecieptDetail recieptDetail = null;
+        Connection conn = DatabaseHelper.getConnect();
+        String sql = "SELECT * FROM reciept_detail WHERE reciept_detail_id = ?";
+        try{
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                recieptDetail = RecieptDetail.fromRS(rs);
+            }
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+            return null;
+        }
+        return recieptDetail;
+    }
+
+    @Override
+    public List<RecieptDetail> getAll() {
+         ArrayList<RecieptDetail> list = new ArrayList();
+        String sql = "SELECT * FROM recipt_detail";
+        Connection conn = DatabaseHelper.getConnect();
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                RecieptDetail receipt = RecieptDetail.fromRS(rs);
+                list.add(receipt);
+
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return list;
+    }
+    @Override
+    public List<RecieptDetail> getAll(String where, String order) {
+        ArrayList<RecieptDetail> list = new ArrayList();
+        String sql = "SELECT * FROM recipt_detail where " + where + " ORDER BY" + order;
+        Connection conn = DatabaseHelper.getConnect();
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                RecieptDetail receipt = RecieptDetail.fromRS(rs);
+                list.add(receipt);
+
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return list;
+    }
+    public List<RecieptDetail> getAll(String order) {
+        ArrayList<RecieptDetail> list = new ArrayList();
+        String sql = "SELECT * FROM recipt_detail  ORDER BY" + order;
+        Connection conn = DatabaseHelper.getConnect();
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                RecieptDetail receipt = RecieptDetail.fromRS(rs);
+                list.add(receipt);
+
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return list;
+    }
+
+
+    @Override
+    public RecieptDetail save(RecieptDetail obj) {
+        String sql = "INSERT INTO reciept_detail(reciept_detail_name,reciept_detail_qty,reciept_detail_price,reciept_detail_size,type_price,type,size_price,topping,topping_price,reciept_detail_total_price,reciept_id,product_id) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
+        Connection conn = DatabaseHelper.getConnect();
+        try{
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, obj.getName());
+            stmt.setInt(2, obj.getQty());
+            stmt.setFloat(3, obj.getPrice());
+            stmt.setString(4, obj.getSize());
+            stmt.setFloat(5, obj.getTypePrice());
+            stmt.setString(6, obj.getType());
+            stmt.setFloat(7, obj.getSizePrice());
+            stmt.setString(8, obj.getTopping());
+            stmt.setFloat(9, obj.getToppingPrice());
+            stmt.setFloat(10, obj.getTotal());
+            stmt.setInt(11, obj.getRecieptId());
+            stmt.setInt(12, obj.getProductId());
+            stmt.executeUpdate();
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+            return null;
+        }
+        return obj;
+    }
+
+    @Override
+    public RecieptDetail update(RecieptDetail obj) {
+        String sql = "UPDATE reciept_detail SET reciept_detail_name = ?,reciept_detail_qty = ?,reciept_detail_price = ?,reciept_detail_size = ?,type_price = ?,type = ?,size_price = ?,topping = ?,topping_price = ?,reciept_detail_total_price = ?,reciept_id = ?,product_id = ? WHERE reciept_detail_id = ?";
+        Connection conn = DatabaseHelper.getConnect();
+        try{
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, obj.getName());
+            stmt.setInt(2, obj.getQty());
+            stmt.setFloat(3, obj.getPrice());
+            stmt.setString(4, obj.getSize());
+            stmt.setFloat(5, obj.getTypePrice());
+            stmt.setString(6, obj.getType());
+            stmt.setFloat(7, obj.getSizePrice());
+            stmt.setString(8, obj.getTopping());
+            stmt.setFloat(9, obj.getToppingPrice());
+            stmt.setFloat(10, obj.getTotal());
+            stmt.setInt(11, obj.getRecieptId());
+            stmt.setInt(12, obj.getProductId());
+            stmt.setInt(13, obj.getId());
+            stmt.executeUpdate();
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+            return null;
+        }
+        return obj;
+    }
+
+    @Override
+    public int delete(RecieptDetail obj) {
+        String sql = "DELETE FROM reciept_detail WHERE reciept_detail_id = ?";
+        Connection conn = DatabaseHelper.getConnect();
+        try{
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, obj.getId());
+            int ret = stmt.executeUpdate();
+            return ret;
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+            return -1;
+        }
+    }
+
+   
+    
+}

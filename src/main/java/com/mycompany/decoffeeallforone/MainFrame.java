2bd0d764d706b5cc6813b7582fc1863c5ce5f5b6
Supanut Thamsaard <64160147@go.buu.ac.th>
2023-10-16 22:31:24 +0700

    HistoryCheckStock / UpdateCheckStock  Dialog

--- src/main/java/com/mycompany/decoffeeallforone/MainFrame.java
+++ src/main/java/com/mycompany/decoffeeallforone/MainFrame.java
@@ -6,7 +6,6 @@ package com.mycompany.decoffeeallforone;
 
 import Component.ProductListPanel;
 import Dialog.CustomerDialog;
-import Page.PromotionMainData;
 import Model.Promotion;
 import Page.PosPanel;
 import Page.ProductPanel;

7efa0dc4669fc08c21ba79d68f5142eaebfe2438
Thanawuth Rodcharoen <64160049@go.buu.ac.th>
2023-10-18 18:56:27 +0700

    finish RentStoreServive

---
+++ src/main/java/Service/RentStoreService.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Service;
+
+
+
+import Dao.MaterialDao;
+import Model.Material;
+import Model.MaterialReport;
+import java.util.List;
+
+/**
+ *
+ * @author werapan
+ */
+public class RentStoreService {
+    static Material currentMaterial;
+
+    public static Material getCurrenMaterial() {
+        return currentMaterial;
+    } 
+    public Material getById(int id) {
+        MaterialDao materialDao = new MaterialDao();
+        return materialDao.get(id);
+    }
+    
+    public List<MaterialReport> getMaterialByMinQty() {
+        MaterialDao materialDao = new MaterialDao();
+        return materialDao.getMaterialByMinQty();
+    }
+    
+    
+    public List<Material> getMaterials(){
+        MaterialDao materialDao = new MaterialDao();
+        return materialDao.getAll(" mat_id asc");
+    }
+
+    public Material addNew(Material editedMaterial) {
+        MaterialDao materialDao = new MaterialDao();
+        return materialDao.save(editedMaterial);
+    }
+
+    public Material update(Material editedMaterial) {
+        MaterialDao materialDao = new MaterialDao();
+        return materialDao.update(editedMaterial);
+    } 
+
+    public int delete(Material editedMaterial) {
+        MaterialDao materialDao = new MaterialDao();
+        return materialDao.delete(editedMaterial);
+    }
+}

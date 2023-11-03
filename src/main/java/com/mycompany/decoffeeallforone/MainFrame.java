51d1b91b7192fc6a0b992159a8b9dab577821f9f
Supanut Thamsaard <64160147@go.buu.ac.th>
2023-11-03 21:19:24 +0700

    move PromotionPanel to Page

--- src/main/java/com/mycompany/decoffeeallforone/MainFrame.java
+++ src/main/java/com/mycompany/decoffeeallforone/MainFrame.java
@@ -29,6 +29,7 @@ import Page.PayRentPanel;
 
 import Page.PosPanel;
 import Page.ProductPanel;
+import Page.PromotionPanel;
 import Page.ReportPanel;
 import Page.SalaryPanel;
 import Page.TablePaymentStatusPanel;
@@ -113,7 +114,7 @@ public class MainFrame extends javax.swing.JFrame implements ChagePage, changePa
 
        //scrPanel.setViewportView(reportPanel);
 
-       scrPanel.setViewportView(new UserPanel());
+       scrPanel.setViewportView(new PromotionPanel());
        
 //       scrPanel.setViewportView(new PosDialog());
 //        PosDialog posDialog = new PosDialog(frame);

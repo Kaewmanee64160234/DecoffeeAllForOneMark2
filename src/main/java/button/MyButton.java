f93140f03974f383207b210b64b552512f9b53e3
Pornchitar Sarsai <kream21574@gmail.com>
2023-11-04 12:44:14 +0700

    testing button

--- src/main/java/button/MyButton.java
+++ src/main/java/button/MyButton.java
@@ -4,126 +4,60 @@
  */
 package button;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridLayout;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 
 /**
  *
  * @author ASUS
  */
-public class MyButton extends JButton{
+
+public class MyButton implements ActionListener {
+    JFrame interfaceFrame;
+    JButton singleplayerButton, multiplayerButton, optionsButton, quitButton;
 
     public MyButton() {
-        //init color
-        color = Color.WHITE;
-        colorOver = new Color(179,250,160);
-        colorClick = new Color(152,184,144);
-        borderColor = new Color(30,136,56);
-        setContentAreaFilled(false);
-        addMouseListener(new MouseAdapter(){
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                setBackground(colorOver);
-                over = true;
-            }
+        JFrame.setDefaultLookAndFeelDecorated(false);
+                interfaceFrame = new JFrame("G-Mine B0.4");
+                interfaceFrame.setSize(800,600);
+                interfaceFrame.setLayout(new GridLayout(9,1, 20, 15));
 
-            @Override
-            public void mouseExited(MouseEvent e) {
-                setBackground(color);
-                over = false;
-            }
+                singleplayerButton = new JButton("SinglePLayer");
+                singleplayerButton.addActionListener(this);
+                interfaceFrame.add(singleplayerButton);
+                singleplayerButton.setBounds(20, 20, 200, 100);
 
-            @Override
-            public void mousePressed(MouseEvent e) {
-                setBackground(colorClick);
-            }
+                multiplayerButton = new JButton("MultiPlayer");
+                multiplayerButton.addActionListener(this);
+                interfaceFrame.add(multiplayerButton);
 
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                if(over){
-                    setBackground(colorOver);
-                }else{
-                    setBackground(color);
-                }
-            }
-            
-            
-        });
-    }
-    
-    private boolean over;
-    private Color color;
-    private Color colorOver;
-    private Color colorClick;
-    private Color borderColor;
-    private int radius = 0;
+                optionsButton = new JButton("Options");
+                optionsButton.addActionListener(this);
+                interfaceFrame.add(optionsButton);
 
-    public boolean isOver() {
-        return over;
-    }
+                quitButton = new JButton("Quit");
+                quitButton.addActionListener(this);
+                interfaceFrame.add(quitButton);
 
-    public void setOver(boolean over) {
-        this.over = over;
-    }
-
-    public Color getColor() {
-        return color;
-    }
-
-    public void setColor(Color color) {
-        this.color = color;
-        setBackground(color);
-    }
-
-    public Color getColorOver() {
-        return colorOver;
-    }
-
-    public void setColorOver(Color colorOver) {
-        this.colorOver = colorOver;
-    }
-
-    public Color getColorClick() {
-        return colorClick;
-    }
-
-    public void setColorClick(Color colorClick) {
-        this.colorClick = colorClick;
-    }
-
-    public Color getBorderColor() {
-        return borderColor;
-    }
-
-    public void setBorderColor(Color borderColor) {
-        this.borderColor = borderColor;
-    }
-
-    public int getRadius() {
-        return radius;
-    }
-
-    public void setRadius(int radius) {
-        this.radius = radius;
+                interfaceFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                interfaceFrame.setVisible(true);
     }
 
     @Override
-    protected void paintComponent(Graphics grphcs) {
-        Graphics2D g2 = (Graphics2D) grphcs;
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        // Paint Border
-        g2.setColor(borderColor);
-        g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
-        g2.setColor(getBackground());
-        //Border set 2 Pix
-        g2.fillRoundRect(2, 2, getWidth()-4, getHeight()-4, radius, radius);
-        super.paintComponent(grphcs); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    public void actionPerformed(ActionEvent e) {
+        
     }
-    
+
     
 }
--- src/main/java/button/Testing.java
+++ src/main/java/button/Testing.java
@@ -4,17 +4,25 @@
  */
 package button;
 
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 /**
  *
  * @author ASUS
  */
-public class Testing extends javax.swing.JFrame {
-
+public class Testing extends javax.swing.JFrame implements ActionListener{
+    JFrame interfaceFrame;
+    JButton singleplayerButton, multiplayerButton, optionsButton, quitButton;
     /**
      * Creates new form Testing
      */
     public Testing() {
         initComponents();
+        
     }
 
     /**
@@ -26,22 +34,64 @@ public class Testing extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(150, 150, 150)
+                .addComponent(jButton1)
+                .addContainerGap(175, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(212, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(65, 65, 65))
         );
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        JFrame.setDefaultLookAndFeelDecorated(false);
+                interfaceFrame = new JFrame("G-Mine B0.4");
+                interfaceFrame.setSize(800,600);
+                interfaceFrame.setLayout(new GridLayout(9,1, 20, 15));
+
+                singleplayerButton = new JButton("SinglePLayer");
+                singleplayerButton.addActionListener(this);
+                interfaceFrame.add(singleplayerButton);
+                singleplayerButton.setBounds(20, 20, 200, 100);
+
+                multiplayerButton = new JButton("MultiPlayer");
+                multiplayerButton.addActionListener(this);
+                interfaceFrame.add(multiplayerButton);
+
+                optionsButton = new JButton("Options");
+                optionsButton.addActionListener(this);
+                interfaceFrame.add(optionsButton);
+
+                quitButton = new JButton("Quit");
+                quitButton.addActionListener(this);
+                interfaceFrame.add(quitButton);
+
+                interfaceFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                interfaceFrame.setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +129,11 @@ public class Testing extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     // End of variables declaration//GEN-END:variables
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }

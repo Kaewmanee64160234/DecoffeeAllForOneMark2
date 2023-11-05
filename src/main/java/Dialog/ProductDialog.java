96bf5e94191897680330870d20352c79c6ebed93
Thanawuth Rodcharoen <64160049@go.buu.ac.th>
2023-11-06 00:22:27 +0700

    fix bug product

--- src/main/java/Dialog/ProductDialog.java
+++ src/main/java/Dialog/ProductDialog.java
@@ -11,6 +11,7 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
@@ -270,6 +271,10 @@ public class ProductDialog extends javax.swing.JDialog {
             JOptionPane.showMessageDialog(this, "Plase Insert name more than 3 character");
             return;
         }
+        if (isDuplicatePName(name)) {
+            JOptionPane.showMessageDialog(this, "This product name is a duplicate.");
+            return;
+        }
         try {
             price = Float.parseFloat(edtPrice.getText());
             if (price < 1) {
@@ -312,7 +317,17 @@ public class ProductDialog extends javax.swing.JDialog {
     private void lblPhotoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblPhotoMouseClicked
         chooseImage();
     }//GEN-LAST:event_lblPhotoMouseClicked
+    private boolean isDuplicatePName(String productName) {
+        List<Product> products = productService.getProductsOrderByName(); // Replace with your data source
 
+        for (Product product : products) {
+            if (product.getName().equals(productName)) {
+                return true; // The phone number is a duplicate
+            }
+        }
+
+        return false; // The phone number is not a duplicate
+    }
     private void cmbCatIdActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmbCatIdActionPerformed
 
     }//GEN-LAST:event_cmbCatIdActionPerformed
